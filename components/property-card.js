8c47bbbeda60ebd7d377415bfb3f9c1f40a6306e
kovalen1618 <kovalen1.618@gmail.com>
2023-07-28 10:10:37 -0500

    Implemented Supabase
    Installed Supabase JS package
    Create .env file with Supabase URL & KEY
    Used createClient() to access database properties table
    Mapped properties to PropertyCard components

--- components/property-card.js
+++ components/property-card.js
@@ -1,58 +1,14 @@
 import { useMemo } from "react";
 
-const PropertyCard = ({
-  imageDimensions,
-  imageDimensionIds,
-  imageDimensionIds2,
-  imageDimensionValues,
-  imageDimensionValues2,
-  imageDimensionPairs,
-  imageDimensionPairIds,
-  imageDimensionPairValues,
-  card1Width,
-  card1Flex,
-  card1MinWidth,
-  card1MaxWidth,
-  shareBoxSizing,
-  favoriteBoxSizing,
-  addBoxSizing,
-}) => {
-  const card12Style = useMemo(() => {
-    return {
-      width: card1Width,
-      flex: card1Flex,
-      minWidth: card1MinWidth,
-      maxWidth: card1MaxWidth,
-    };
-  }, [card1Width, card1Flex, card1MinWidth, card1MaxWidth]);
-
-  const shareStyle = useMemo(() => {
-    return {
-      boxSizing: shareBoxSizing,
-    };
-  }, [shareBoxSizing]);
-
-  const favoriteStyle = useMemo(() => {
-    return {
-      boxSizing: favoriteBoxSizing,
-    };
-  }, [favoriteBoxSizing]);
-
-  const addStyle = useMemo(() => {
-    return {
-      boxSizing: addBoxSizing,
-    };
-  }, [addBoxSizing]);
-
+const PropertyCard = ({ imgUrl, name, price }) => {
   return (
     <div
       className="rounded-3xs bg-gray-white box-border w-[350px] h-[466.99px] flex flex-col py-[15px] px-[13px] items-start justify-start gap-[23px] text-left text-[14.51px] text-gray-700 font-body-large-400 border-[1px] border-solid border-whitesmoke-100 hover:bg-gainsboro hover:cursor-pointer sm:w-[100%!important] sm:mb-5"
-      style={card12Style}
     >
       <img
         className="self-stretch relative rounded-3xs max-w-full overflow-hidden h-[200px] shrink-0 object-cover"
         alt=""
-        src={imageDimensions}
+        src={imgUrl}
       />
       <div className="self-stretch flex flex-row p-2.5 items-start justify-start">
         <div className="flex-1 relative leading-[20.32px] font-medium">
@@ -64,28 +20,28 @@ const PropertyCard = ({
           $ 590,693
         </div>
       </div>
-      <div className="self-stretch flex flex-row py-0 px-2.5 items-start justify-start gap-[17px] text-[10.84px] text-gray-500">
+      <div className="flex flex-row py-0 px-2.5 items-start justify-start gap-[17px] text-[10.84px] text-gray-500">
         <div className="flex flex-row items-center justify-start gap-[4.35px]">
           <img
-            className="relative w-[17.42px] h-[17.42px]"
+            className="w-[17.42px] h-[17.42px]"
             alt=""
-            src={imageDimensionIds}
+            src="/car.svg"
           />
           <div className="relative leading-[16.27px] font-medium">4</div>
         </div>
         <div className="flex flex-row items-center justify-start gap-[4.35px]">
           <img
-            className="relative w-[17.42px] h-[17.42px]"
+            className="w-[17.42px] h-[17.42px]"
             alt=""
-            src={imageDimensionIds2}
+            src="/bathtub.svg"
           />
           <div className="relative leading-[16.27px] font-medium">4</div>
         </div>
         <div className="flex flex-row items-center justify-start gap-[4.35px]">
           <img
-            className="relative w-[17.42px] h-[17.42px]"
+            className="w-[17.42px] h-[17.42px]"
             alt=""
-            src={imageDimensionValues}
+            src="/arrowsout.svg"
           />
           <div className="relative leading-[16.27px] font-medium">
             2,096.00 ft
@@ -95,9 +51,9 @@ const PropertyCard = ({
       <div className="self-stretch flex flex-row items-center justify-between text-center text-[11.61px]">
         <div className="flex flex-row items-center justify-start gap-[5.81px]">
           <img
-            className="relative rounded-[50%] w-[27.57px] h-[27.57px] object-cover"
+            className="rounded-[50%] w-[27.57px] h-[27.57px] object-cover"
             alt=""
-            src={imageDimensionValues2}
+            src="/ellipse-1@2x.png"
           />
           <div className="relative leading-[17.42px] font-medium">
             Jenny Wilson
@@ -106,32 +62,29 @@ const PropertyCard = ({
         <div className="flex flex-row items-start justify-start gap-[8.71px]">
           <div
             className="rounded-[1.45px] bg-primary-50 flex flex-row p-[2.902620315551758px] items-start justify-start"
-            style={shareStyle}
           >
             <img
-              className="relative w-[14.51px] h-[14.51px]"
+              className="w-[14.51px] h-[14.51px]"
               alt=""
-              src={imageDimensionPairs}
+              src="/sharenetwork.svg"
             />
           </div>
           <div
             className="rounded-[1.45px] bg-primary-50 flex flex-row p-[2.902620315551758px] items-start justify-start"
-            style={favoriteStyle}
           >
             <img
-              className="relative w-[14.51px] h-[14.51px]"
+              className="w-[14.51px] h-[14.51px]"
               alt=""
-              src={imageDimensionPairIds}
+              src="/heart.svg"
             />
           </div>
           <div
             className="rounded-[1.45px] bg-primary-50 flex flex-row p-[2.902620315551758px] items-start justify-start"
-            style={addStyle}
           >
             <img
-              className="relative w-[14.51px] h-[14.51px]"
+              className="w-[14.51px] h-[14.51px]"
               alt=""
-              src={imageDimensionPairValues}
+              src="/plus.svg"
             />
           </div>
         </div>
